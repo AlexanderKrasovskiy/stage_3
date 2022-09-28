af98f419bde85517a951b17287fd8e0347984f70
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-09-28 12:37:40 +0600

    feat: generate components for main page

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FiltersComponent } from './header/filters/filters.component';
+import { AuthComponent } from './main/auth/auth.component';
+import { SearchResultsComponent } from './main/search/search-results/search-results.component';
+import { SearchCardComponent } from './main/search/search-card/search-card.component';
+import { SearchDetailsComponent } from './main/search/search-details/search-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FiltersComponent,
+    AuthComponent,
+    SearchResultsComponent,
+    SearchCardComponent,
+    SearchDetailsComponent
   ],
   imports: [
     BrowserModule,
