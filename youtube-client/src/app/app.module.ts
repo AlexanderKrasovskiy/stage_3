d1efab888e0330d8dc49c276d76baf305233380b
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-09-30 14:37:27 +0600

    refactor: reorganise components into more flat structure

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FiltersComponent } from './header/filters/filters.component';
-import { AuthComponent } from './main/auth/auth.component';
-import { SearchListComponent } from './main/search/search-list/search-list.component';
-import { SearchCardComponent } from './main/search/search-card/search-card.component';
-import { SearchDetailsComponent } from './main/search/search-details/search-details.component';
+import { AuthComponent } from './auth/auth.component';
+import { SearchListComponent } from './search/search-list/search-list.component';
+import { SearchCardComponent } from './search/search-card/search-card.component';
+import { SearchDetailsComponent } from './search/search-details/search-details.component';
 
 @NgModule({
   declarations: [
