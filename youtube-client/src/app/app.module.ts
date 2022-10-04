9e2d194771d1084483a218dc59cb158391366443
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-04 14:27:41 +0600

    feat: add Angular Material library

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,7 +17,7 @@ import { SearchCardComponent } from './search-list/search-card/search-card.compo
     SearchListComponent,
     SearchCardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
