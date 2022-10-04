4b9e5fd7b2370138d5e50997f085c120d61adc83
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-04 11:41:46 +0600

    refactor: move filters from header folder one level up

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FiltersComponent } from './header/filters/filters.component';
+import { FiltersComponent } from './filters/filters.component';
 import { AuthComponent } from './auth/auth.component';
 import { SearchListComponent } from './search/search-list/search-list.component';
 import { SearchCardComponent } from './search/search-card/search-card.component';
