b5e609cefd38fa9d1e0c06595ed8558ce265f742
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-05 11:02:43 +0600

    refactor: move material module into modules folder

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './modules/material/material.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
