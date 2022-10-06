49e0034351508d3526d23cedffc4d06346c9f8e2
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-06 17:31:41 +0600

    refactor: add reset header form in .ts file instead of .html

--- youtube-client/src/app/components/header/header.component.ts
+++ youtube-client/src/app/components/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +15,7 @@ export class HeaderComponent {
   public query: string = '';
   @Output() public search = new EventEmitter<string>();
   @Output() public toggle = new EventEmitter();
+  @ViewChild('searchForm') searchForm!: ElementRef<HTMLFormElement>;
 
   public onSearch() {
     this.query = this.query.trim();
@@ -16,6 +23,7 @@ export class HeaderComponent {
     if (this.query) {
       this.search.emit(this.query.toLowerCase());
     }
+    this.searchForm.nativeElement.reset();
   }
 
   public toggleFilters() {
