1c47bbc4a92cf357ea821f5f20196a3b66184694
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-07 20:16:05 +0600

    refactor: implement shorter solution it sort-by-views pipe

--- youtube-client/src/app/pipes/sort-by-views.pipe.ts
+++ youtube-client/src/app/pipes/sort-by-views.pipe.ts
@@ -8,19 +8,11 @@ export class SortByViewsPipe implements PipeTransform {
   transform(items: YtItem[], order?: 'ascending' | 'descending'): YtItem[] {
     if (!order) return items;
 
-    if (order === 'ascending') {
-      return [...items].sort((a, b) => {
-        const viewsA = +a.statistics.viewCount;
-        const viewsB = +b.statistics.viewCount;
-
-        return viewsA - viewsB;
-      });
-    }
     return [...items].sort((a, b) => {
       const viewsA = +a.statistics.viewCount;
       const viewsB = +b.statistics.viewCount;
 
-      return viewsB - viewsA;
+      return order === 'ascending' ? viewsA - viewsB : viewsB - viewsA;
     });
   }
 }
