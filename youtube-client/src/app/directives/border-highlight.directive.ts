02ca29e1e0db34e2fc58d5743a0bce40684870fe
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-07 18:19:22 +0600

    refactor: BorderHighlightDirective - rename date variable and add default behavior

--- youtube-client/src/app/directives/border-highlight.directive.ts
+++ youtube-client/src/app/directives/border-highlight.directive.ts
@@ -5,7 +5,7 @@ import { CardBottomBorderColors } from '../constants';
   selector: '[appBorderHighlight]',
 })
 export class BorderHighlightDirective implements OnInit {
-  @Input() date!: string;
+  @Input() dateOfPublishing?: string;
   private color?: CardBottomBorderColors;
 
   constructor(private elementRef: ElementRef, private renderer2: Renderer2) {}
@@ -21,21 +21,25 @@ export class BorderHighlightDirective implements OnInit {
   }
 
   private resolveBottomColor(): void {
-    const todayStamp = new Date().getTime();
-    const publishStamp = new Date(this.date).getTime();
-
-    const dayInMs = 1000 * 60 * 60 * 24;
-    const diffMs = todayStamp - publishStamp;
-    const days = Math.floor(diffMs / dayInMs);
-
-    if (days > 182) {
+    if (!this.dateOfPublishing) {
       this.color = CardBottomBorderColors.Red;
-    } else if (days >= 30) {
-      this.color = CardBottomBorderColors.Yellow;
-    } else if (days > 7) {
-      this.color = CardBottomBorderColors.Green;
     } else {
-      this.color = CardBottomBorderColors.Blue;
+      const todayStamp = new Date().getTime();
+      const publishStamp = new Date(this.dateOfPublishing).getTime();
+
+      const dayInMs = 1000 * 60 * 60 * 24;
+      const diffMs = todayStamp - publishStamp;
+      const days = Math.floor(diffMs / dayInMs);
+
+      if (days > 182) {
+        this.color = CardBottomBorderColors.Red;
+      } else if (days >= 30) {
+        this.color = CardBottomBorderColors.Yellow;
+      } else if (days > 7) {
+        this.color = CardBottomBorderColors.Green;
+      } else {
+        this.color = CardBottomBorderColors.Blue;
+      }
     }
   }
 }
