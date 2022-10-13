ce4d702c972a9883298f43c77654408294e421b2
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-13 23:20:07 +0600

    refactor: move search-list to pages

--- youtube-client/src/app/youtube/youtube.module.ts
+++ youtube-client/src/app/youtube/youtube.module.ts
@@ -4,14 +4,13 @@ import { YoutubeRoutingModule } from './youtube-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
 
-import { SearchListComponent } from './components/search-list/search-list.component';
+import { SearchListComponent } from './pages/search-list/search-list.component';
 import { SearchCardComponent } from './components/search-card/search-card.component';
 
 import { BorderHighlightDirective } from './directives/border-highlight.directive';
 import { SortByDatePipe } from './pipes/sort-by-date.pipe';
 import { SortByViewsPipe } from './pipes/sort-by-views.pipe';
 import { FilterByWordPipe } from './pipes/filter-by-word.pipe';
-import { MainPageComponent } from './pages/main-page/main-page.component';
 import { DetailsPageComponent } from './pages/details-page/details-page.component';
 
 @NgModule({
@@ -22,10 +21,9 @@ import { DetailsPageComponent } from './pages/details-page/details-page.componen
     SortByDatePipe,
     SortByViewsPipe,
     FilterByWordPipe,
-    MainPageComponent,
     DetailsPageComponent,
   ],
   imports: [CommonModule, YoutubeRoutingModule, SharedModule],
-  exports: [SearchListComponent], // m.b. refactor to page ?
+  exports: [SearchListComponent],
 })
 export class YoutubeModule {}
