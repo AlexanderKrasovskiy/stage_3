d84aa287a878047820bcee7f64d15aa388f6221f
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-14 19:50:43 +0600

    feat: add routing

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule } from './core/core.module';
-import { YoutubeModule } from './youtube/youtube.module'; // move to Router + Auth
-import { AuthModule } from './auth/auth.module'; // move to Router + Auth
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -16,8 +13,6 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
-    YoutubeModule, // move to Router + Auth
-    AuthModule, // move to Router + Auth
   ],
   providers: [],
   bootstrap: [AppComponent],
--- youtube-client/src/app/youtube/youtube-routing.module.ts
+++ youtube-client/src/app/youtube/youtube-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchListComponent } from './pages/search-list/search-list.component';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchListComponent },
+  { path: ':id', component: DetailsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
