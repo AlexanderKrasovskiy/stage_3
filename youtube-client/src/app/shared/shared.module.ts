8f18caf067fd92ff477a40406c342f6b2d708c23
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-10-30 11:56:00 +0600

    refactor: remove FormsModule from shared.module

--- youtube-client/src/app/shared/shared.module.ts
+++ youtube-client/src/app/shared/shared.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material/material.module';
 import { VideoStatsComponent } from './components/video-stats/video-stats.component';
 
 @NgModule({
   declarations: [VideoStatsComponent],
-  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
-  exports: [
-    MaterialModule,
-    VideoStatsComponent,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
+  exports: [MaterialModule, VideoStatsComponent, ReactiveFormsModule],
 })
 export class SharedModule {}
